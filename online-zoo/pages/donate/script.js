95ab6d006f4fbee00f51dd1f2aa2af3e86e5f27a
Darya Suravtsova <darblnkapm@gmail.com>
2022-10-13 03:57:10 +0300

    fix: struct of panel at donate page; feat: panel at donate page

---
+++ online-zoo/pages/donate/script.js
@@ -0,0 +1,61 @@
+let brg = document.getElementsByClassName('burger');
+let head = document.getElementsByClassName('header');
+let cont = document.getElementsByClassName('container');
+let nav = document.getElementsByClassName('text_of_header');
+let log = document.getElementsByClassName('logo');
+let image = document.getElementsByClassName('bamboo');
+
+brg[0].addEventListener('click', function() {
+    head[0].classList.contains("white_container") ? closeBurgerMenu() : openBurgerMenu()
+})
+
+function openBurgerMenu () {
+    nav[0].classList.add("nav_of_burger")
+    head[0].classList.add("white_container")
+    cont[0].classList.add("burger_container")
+    image[0].src = "../../assets/images/bamboo_orange.svg"
+}
+
+function closeBurgerMenu () {
+    nav[0].classList.remove("nav_of_burger")
+    head[0].classList.remove("white_container")
+    cont[0].classList.remove("burger_container")
+    image[0].src = "../../assets/images/bamboo.svg"
+}
+
+let cont_of_points = document.querySelector('.points')
+let points = document.getElementsByClassName('point')
+let numbers = document.querySelectorAll('#money p')
+let input = document.querySelector('#number1')
+
+cont_of_points.addEventListener('click', function(e) {
+    let i = Array.from(points).indexOf(e.target)
+    Array.from(points).forEach(point => {
+        point.classList.remove('active_point')
+    });
+    points[i].classList.add('active_point')
+    Array.from(numbers).forEach(number => {
+        number.classList.remove('active_number')
+    });
+    numbers[i].classList.add('active_number')
+    input.value = parseInt(numbers[i].innerHTML.replace('$', ''))
+})
+
+input.oninput = function() {
+    if (input.value.length > 4)
+        input.value = input.value.substr(0, 4)
+    let qua = 0;
+    numbers.forEach(number => {
+        if (parseInt(number.innerHTML.replace('$', '')) == input.value) {
+            numbers.forEach(number => {
+                number.classList.remove('active_number')
+            });
+            number.classList.add('active_number')
+            Array.from(points).forEach(point => {
+                point.classList.remove('active_point')
+            });
+            points[qua].checked = true
+        }
+        qua++
+    })
+}
--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -1,15 +1,13 @@
-let brg = document.getElementsByClassName('burger')
-let head = document.getElementsByClassName('header')
-let cont = document.getElementsByClassName('container')
-let nav = document.getElementsByClassName('text_of_header')
+let brg = document.getElementsByClassName('burger');
+let head = document.getElementsByClassName('header');
+let cont = document.getElementsByClassName('container');
+let nav = document.getElementsByClassName('text_of_header');
 let log = document.getElementsByClassName('logo');
 let image = document.getElementsByClassName('bamboo');
 
-brg[0].addEventListener('click', check)
-
-function check () {
+brg[0].addEventListener('click', function() {
     head[0].classList.contains("white_container") ? closeBurgerMenu() : openBurgerMenu()
-}
+})
 
 function openBurgerMenu () {
     nav[0].classList.add("nav_of_burger")
@@ -23,4 +21,33 @@ function closeBurgerMenu () {
     head[0].classList.remove("white_container")
     cont[0].classList.remove("burger_container")
     image[0].src = "../../assets/images/bamboo.svg"
+}
+
+let pet = document.querySelectorAll('.pet');
+let cor = document.querySelector('#pets .content')
+
+document.querySelector('#left').addEventListener('click', clickLeftButton)
+document.querySelector('#right').addEventListener('click', clickRightButton)
+
+function generatePage() {
+    let cont = document.createElement('div')
+    cont.classList.add('content_of_generate')
+    let row1 = document.createElement('div')
+    row1.classList.add('row_of_pets')
+    let row2 = document.createElement('div')
+    row2.classList.add('row_of_pets')
+    row1.append(pet[0].cloneNode(true), pet[1].cloneNode(true), pet[2].cloneNode(true))
+    row2.append(pet[3].cloneNode(true), pet[4].cloneNode(true), pet[5].cloneNode(true))
+    cont.append(row1, row2)
+    return cont
+}
+
+function clickLeftButton () {
+    let newPage = generatePage()
+    cor.append(newPage)
+    newPage.addEventListener('')
+}
+
+function clickRightButton () {
+
 }
